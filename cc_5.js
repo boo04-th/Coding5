f17e7698ecbc812aebd965f77901674823fb6f15
Hient <hient@usf.edu>
2025-02-09 00:53:54 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+//Task 1: Object Profile
+//Declare an object customer
+const customer = { 
+    name:"John Doe",
+    age: 35,
+    email:"john.doe@email.com"
+};
+console.log(`Name: ${customer.name}`); //Logging name of customer
+console.log(`Age: ${customer.age}`); // Logging age of customer
+console.log(`Email: ${customer.email}`); //logging email of customer
