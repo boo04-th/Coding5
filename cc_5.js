6882b59d09c9ac9c9c26254e0a00369394db3fc7
Hient <hient@usf.edu>
2025-02-09 00:57:28 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -1,5 +1,5 @@
 //Task 1: Object Profile
-//Declare an object customer
+//Declaring an object customer
 const customer = { 
     name:"John Doe",
     age: 35,
@@ -8,3 +8,15 @@ const customer = {
 console.log(`Name: ${customer.name}`); //Logging name of customer
 console.log(`Age: ${customer.age}`); // Logging age of customer
 console.log(`Email: ${customer.email}`); //logging email of customer
+
+//Task 2: Object Methods
+//Declaring an object order
+const order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder() {
+      console.log(`Order ID: ${this.orderId}, Total Amount: $${this.totalAmount}, Status: ${this.status}`);
+    }
+  }; //Console log of customer order 
+  order.displayOrder();
