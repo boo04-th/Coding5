fa3ce093b3e846cde8ca9c267c2a996764425e12
Hient <hient@usf.edu>
2025-02-09 21:56:44 -0500

    Task 4 - Price Adjustments

--- cc_5.js
+++ cc_5.js
@@ -15,17 +15,23 @@ const order = {
     orderId: 12345,
     totalAmount: 150,
     status: "Processing",
-    displayOrder() {
+    displayOrder: function() {
       console.log(`Order ID: ${this.orderId}, Total Amount: $${this.totalAmount}, Status: ${this.status}`);
     }
   }; //Console log of customer order 
   order.displayOrder();
 
-  //Task 3: Array Manipulation (push, pop, shift, unshift)
-  let cartItems = ["Newport", "Kool", "Clipper"]; // Array of items
+//Task 3: Array Manipulation (push, pop, shift, unshift)
+let cartItems = ["Newport", "Kool", "Clipper"]; // Array of items
   cartItems.push("Salem"); // Adding new item
   cartItems.pop(); // Removing last item
   cartItems.unshift("Westport"); // Adding item to beginning
-  cartItems.shift(); // removing beginning item
-  console.log(cartItems);
+  cartItems.shift(); // Removing beginning item
+  console.log("Final list of the Cart Items:",cartItems);
   
+//Task 4: Map Method
+const prices = [100, 200, 300]; // Array of prices
+const discountedPrices = prices.map(price => price * 0.9); //10% discount
+console.log("Discounted Prices:", discountedPrices);
+
+
